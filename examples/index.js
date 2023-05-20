8e680a0ddbdb9a5d0acec640315a810503c15e65
Michael Di Prisco <cadienvan@gmail.com>
2023-05-20 15:13:23 +0200

    chore: removing examples

--- examples/index.js
+++
@@ -1,7 +0,0 @@
-const { setTimeoutById } = require('../dist/index.js')
-
-console.log('before setTimeoutById');
-setTimeoutById(() => {
-  console.log('in setTimeoutById');
-}, 1000, 'myTimeout');
-console.log('after setTimeoutById');
